4d1e943cfd2ceb7b232f2fbd72ceede5073c5155
progamer63 <geometry1224gd@gmail.com>
2023-07-10 13:02:30 -0700

    Update acceptedVers.py

--- acceptedVers.py
+++ acceptedVers.py
@@ -1,5 +1,6 @@
 versList = {
     '0.10.5':'https://github.com/EliteAsian123/YARG/releases/download/v0.10.5/YARG_v0.10.5-Windows-x64.zip',
     '0.10.6':'https://github.com/EliteAsian123/YARG/releases/download/v0.10.6/YARG_v0.10.6-Windows-x64.zip',
-    '0.10.7':'https://github.com/YARC-Official/YARG/releases/download/v0.10.7/YARG_v0.10.7-Windows-x64.zip'
+    '0.10.7':'https://github.com/YARC-Official/YARG/releases/download/v0.10.7/YARG_v0.10.7-Windows-x64.zip',
+    '0.11.1':'https://github.com/YARC-Official/YARG/releases/download/v0.11.0/YARG_v0.11.0-Windows-x64.zip'
     }
